a0ec93627f97f6edea22eaed78dd96f15aeb25f2
ftem2002 <91579449+ftem2002@users.noreply.github.com>
2021-12-23 21:36:39 +0300

    Add files via upload

---
+++ main my 1.py
@@ -0,0 +1,26 @@
+text = "fatima"
+first_name ="ali"
+last_name = "mon"
+print("fat\nima")
+print("fat\tima")
+print(text.upper().isupper())
+print(len("dog"))
+print(text[0])
+print(last_name[2],first_name[2])
+print(text.index("a"))
+print(text.replace("at","y"))
+print(7890)
+my_num = 5
+print(pow(6,4))
+name = "fatima"
+name = input("enter your name:")
+print("hello "+ name)
+print(len(text))
+
+num1 = input("enter the first no:")
+num2 = input("enter the second no:")
+result=float(num1)+float(num2)
+print(result)
+
+
+
